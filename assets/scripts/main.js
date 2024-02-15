6a6c288a1a1b39edfdb50059a833922e9658bd14
kazuki higashi <kazukihigashi@kazukiMacBook-Air-M2.local>
2024-02-16 01:55:26 +0900

    ハンバーガー機能せず

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 // console.log( 'JSを読み込んだよ！' );
 {
-const hamburger = document.querySelector('.p-btn-line'); // 要素取得して変数に格納
-const nav = document.querySelector('.l-sidebar'); 
+const hamburger = document.querySelector('.p-js-btn--line'); // 要素取得して変数に格納
+const nav = document.querySelector('.p-js-btn'); 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("open");//処理内容
     nav.classList.toggle('open'); //openクラスを付け外しする
