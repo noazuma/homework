d4e261841d67b6ecc766bf40e558f9ec4a467547
kazuki higashi <kazukihigashi@kazukiMacBook-Air-M2.local>
2024-02-16 16:19:17 +0900

    静的コーディング一旦完了

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 // console.log( 'JSを読み込んだよ！' );
 {
-const hamburger = document.querySelector('.p-js-btn--line'); // 要素取得して変数に格納
-const nav = document.querySelector('.p-js-btn'); 
+const hamburger = document.querySelector('.js-btn--line'); // 要素取得して変数に格納
+const nav = document.querySelector('.js-nav'); 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("open");//処理内容
     nav.classList.toggle('open'); //openクラスを付け外しする
