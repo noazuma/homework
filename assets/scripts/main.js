19f3572191a5070a7f56ae1b8762e6f4d44b62ad
kazuki higashi <kazukihigashi@kazukiMacBook-Air-M2.local>
2024-02-18 23:10:59 +0900

    マークアップ

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 // console.log( 'JSを読み込んだよ！' );
 {
-const hamburger = document.querySelector('.js-btn--line'); // 要素取得して変数に格納
-const nav = document.querySelector('.js-nav'); 
+const hamburger = document.querySelector('.jsbtn'); // 要素取得して変数に格納
+const nav = document.querySelector('.jsnav'); 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("open");//処理内容
     nav.classList.toggle('open'); //openクラスを付け外しする
